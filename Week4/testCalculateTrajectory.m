06ebf1248c69906830cd9f6066b41cc7f34e78f8
Geoff Berl <gberl001@gmail.com>
2024-09-20 12:17:47 -0400

    Fixing bug in function, various other bug fixes

--- Week4/testCalculateTrajectory.m
+++ Week4/testCalculateTrajectory.m
@@ -1,18 +1,24 @@
 function testCalculateTrajectory()
-    % Test case 1: Basic scenario with velocity = 50 m/s, angle = 45 degrees
+    % Test case 1: Basic scenario with velocity = 50 m/s, angle = 45 degrees, initial height = 10 meters
     v0 = 50;
     angle = 45;
     g = 9.81;
+    y0 = 10;
     numValues = 100;
     
-    [x, y, time] = calculateTrajectory(v0, angle, g, numValues);
+    [x, y, time] = calculateTrajectory(v0, angle, y0, g, numValues);
     
     % Test that the time values are non-negative and increase monotonically
     assert(all(time >= 0), 'Error: Time values should be non-negative');
     assert(issorted(time), 'Error: Time values should be monotonically increasing');
     
-    % Test that x and y values are non-negative and that y hits zero at the end
+    % Test that x values are non-negative
     assert(all(x >= 0), 'Error: Horizontal position values should be non-negative');
+    
+    % Test that y values are greater than or equal to the initial height at the start
+    assert(y(1) >= y0, 'Error: Initial vertical position should be greater than or equal to the initial height');
+    
+    % Test that y values do not fall below zero (projectile does not go underground)
     assert(all(y >= 0), 'Error: Vertical position values should be non-negative');
     
     % Ensure that the final y value is near zero (projectile hitting the ground)
